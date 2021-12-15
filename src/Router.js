7b31f1b4ccfe2921991c9b6cd5cc7f18d8fc1909
KimJeongHyun <ptr1129@hanmail.net>
2021-12-15 16:54:31 +0900

    ADD: Router.js 수정

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginHeewon from './pages/heewonKang/Login/Login';
+import MainHeewon from './pages/heewonKang/Main/Main';
+
+import LoginYongsun from './pages/yongsunKim/Login/Login';
+import MainYongsun from './pages/yongsunKim/Main/Main';
+
+import LoginJeonghyun from './pages/jeonghyunKim/Login/Login';
+import MainJeonghyun from './pages/jeonghyunKim/Main/Main';
+
+import LoginYunkuk from './pages/yunkukPark/Login/Login';
+import MainYunkuk from './pages/yunkukPark/Main/Main';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/login-won" element={<LoginHeewon />} />
+        <Route path="/main-won" element={<MainHeewon />} />
+
+        <Route path="/login-sun" element={<LoginYongsun />} />
+        <Route path="/main-sun" element={<MainYongsun />} />
+
+        <Route path="/login-hyun" element={<LoginJeonghyun />} />
+        <Route path="/main-hyun" element={<MainJeonghyun />} />
+
+        <Route path="/login-kuk" element={<LoginYunkuk />} />
+        <Route path="/main-kuk" element={<MainYunkuk />} />
       </Routes>
     </BrowserRouter>
   );
