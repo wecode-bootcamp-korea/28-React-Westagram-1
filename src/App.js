1f58aa8fc069a63ba90de88fbf7ca92fe6f2c7ee
KimJeongHyun <ptr1129@hanmail.net>
2021-12-15 16:22:47 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
