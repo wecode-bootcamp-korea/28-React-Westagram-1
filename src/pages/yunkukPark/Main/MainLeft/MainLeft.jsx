a6910e5fc388f0fb97ecad30103583a60148f249
YunKukPark <a01043340999@gmail.com>
2021-12-21 10:14:08 +0900

    Add: Mock Data 추가 후 데이터 패칭

--- src/pages/yunkukPark/Main/MainLeft/MainLeft.jsx
+++ src/pages/yunkukPark/Main/MainLeft/MainLeft.jsx
@@ -1,50 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Feed from '../Feed/Feed';
 import Story from '../Story/Story';
 
 const MainLeft = () => {
-  const feedContents = [
-    {
-      feedId: 0,
-      userName: 'seha_park._.',
-      userAvatar: './assets/yunkuk/image/yunkuk-avatar.jpg',
-      feedImg: './assets/yunkuk/image/feedImage1.jpeg',
-      content: '맛있다 💪🏻',
-      likeHit: 123,
-      comments: [
-        {
-          id: 1,
-          userName: 'yunkukpark',
-          content: '와 개미쳤따',
-        },
-        {
-          id: 2,
-          userName: 'parkyun',
-          content: '지리네...',
-        },
-      ],
-    },
-    {
-      feedId: 1,
-      userName: 'ctrsound',
-      userAvatar: './assets/yunkuk/image/storyImage06.jpg',
-      feedImg: './assets/yunkuk/image/feedImage02.jpg',
-      content: `[오늘의 난 미지근하게 축제] 음반 판매 오픈! 🎉`,
-      likeHit: 142,
-      comments: [
-        {
-          id: 1,
-          userName: 'sehapark',
-          content: '안녕',
-        },
-        {
-          id: 2,
-          userName: 'parkyun',
-          content: '하세요',
-        },
-      ],
-    },
-  ];
+  const DATA_URL = 'http://localhost:3000/dataYunkuk';
+  const [feedContents, setfeedContents] = useState([]);
+
+  useEffect(() => {
+    fetch(`${DATA_URL}/feed.json`)
+      .then(res => res.json())
+      .then(data => setfeedContents(data));
+  }, []);
 
   return (
     <div className="main-left">
