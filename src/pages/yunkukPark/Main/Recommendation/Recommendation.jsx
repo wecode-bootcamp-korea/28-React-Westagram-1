fe086d86d7a14443ca4aac82f96f76e1dc83e7e2
YunKukPark <a01043340999@gmail.com>
2021-12-25 20:33:08 +0900

    Fix: misspell 해결

--- src/pages/yunkukPark/Main/Recommendation/Recommendation.jsx
+++ src/pages/yunkukPark/Main/Recommendation/Recommendation.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import RecommnadationList from './RecommendationList';
+import RecommendationList from './RecommendationList';
 
 const Recommendation = () => {
-  const recommadations = [
+  const recommendations = [
     {
       key: 0,
       avatarImage: './assets/yunkuk/image/recommend01.jpg',
       userName: 'suuuuul.p',
-      recommandState: 'Instagram에 새로 가입했습니다',
+      recommendState: 'Instagram에 새로 가입했습니다',
     },
     {
       key: 1,
       avatarImage: './assets/yunkuk/image/recommend02.jpg',
       userName: 'h_sejin_',
-      recommandState: '홍세진',
+      recommendState: '홍세진',
     },
     {
       key: 2,
       avatarImage: './assets/yunkuk/image/recommend03.jpg',
       userName: 'caffein4ddicted',
-      recommandState: 'april__o6님 외 12명이 팔로우합니다',
+      recommendState: 'april__o6님 외 12명이 팔로우합니다',
     },
     {
       key: 3,
       avatarImage: './assets/yunkuk/image/avatar-default.jpg',
       userName: '_terryyomi',
-      recommandState: 'king_sejong님 외 3명이 팔로우합니다',
+      recommendState: 'king_sejong님 외 3명이 팔로우합니다',
     },
     {
       key: 4,
       avatarImage: './assets/yunkuk/image/avatar-default.jpg',
       userName: 'kkrubbingbbong',
-      recommandState:
+      recommendState:
         '굉장히많은글자수가 팔로우를 하는데 넘어가는거는 일랩시스처리합니다',
     },
   ];
@@ -44,9 +44,9 @@ const Recommendation = () => {
         <Link to="#">모두 보기</Link>
       </header>
       <ul className="recommendation-list">
-        {recommadations.map(recommadation => {
-          const { key, ...otherprops } = recommadation;
-          return <RecommnadationList key={key} {...otherprops} />;
+        {recommendations.map(recommendation => {
+          const { key, ...otherprops } = recommendation;
+          return <RecommendationList key={key} {...otherprops} />;
         })}
       </ul>
     </section>
